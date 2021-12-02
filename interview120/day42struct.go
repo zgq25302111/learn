3f6e332440641684ac7462253a96bd0033524086
happyjack <zgq25302111@126.com>
2021-12-03 07:33:34 +0800

    Add files via upload

---
+++ interview120/day42struct.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+type ConfigOne struct {
+	Daemon string
+}
+func (c *ConfigOne) String() string {
+	return fmt.Sprintf("print: %v", c)
+}
+func main() {
+	c := &ConfigOne{}
+	c.String()
+}
