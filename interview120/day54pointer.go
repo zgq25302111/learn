df63bdfb68799263a82abd17361757290a543d2e
happyjack <zgq25302111@126.com>
2022-01-03 22:57:55 +0800

    Add files via upload

---
+++ interview120/day54pointer.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type N int
+
+func main() {
+	var n N = 10
+	fmt.Println(n)
+	n++
+	N.test(n)
+	n++
+	(*N).test(&n)
+}
+
+func (n N) test(){
+	fmt.Println(n)
+}
+/*
+func main() {
+	var n N = 10
+	fmt.Println(n)
+	n++
+	f1 := N.test
+	f1(n)
+	n++
+	f2 := (*N).test
+	f2(&n)
+}*/
