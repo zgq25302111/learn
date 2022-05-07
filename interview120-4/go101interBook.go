f6609846caf751e24cb8a444937d37604f847a49
happyjack <zgq25302111@126.com>
2022-05-07 09:19:40 +0800

    Add files via upload

---
+++ interview120-4/go101interBook.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Aboutable interface {
+	About() string
+}
+
+// Type *Book implements Aboutable.
+type Book struct {
+	name string
+}
+func (book *Book) About() string {
+	return "Book: " + book.name
+}
+
+func main() {
+	// A *Book value is boxed into an
+	// interface value of type Aboutable.
+	var a Aboutable = &Book{"Go 101"}
+	fmt.Println(a) // &{Go 101}
+
+	// i is a blank interface value.
+	var i interface{} = &Book{"Rust 101"}
+	fmt.Println(i) // &{Rust 101}
+
+	// Aboutable implements interface{}.
+	i = a
+	fmt.Println(i) // &{Go 101}
+}
